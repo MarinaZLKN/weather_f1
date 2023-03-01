b6cce5c116ba1a15d8e2ce9bd8c25f616cd18fe0
Marina <marina.zilkina@gmail.com>
2023-03-01 20:09:47 +0200

    Day Compomnet's css is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Search } from './components/search/search';
+import { Day } from './components/day/day';
 
 function App() {
   return (
         <div className="container">
             <Search />
-            Привет
+            <Day />
     </div>
 );
 }
--- src/components/day/day.tsx
+++ src/components/day/day.tsx
@@ -1,1 +1,26 @@
-export {}
+import React from 'react';
+import './day.styles.scss';
+const logo = 'img/sun.png';
+const alt = 'sun';
+
+
+interface Props {
+}
+
+export const Day = (props: Props) => {
+    return (
+        <div className="day_container">
+            <div className="upper_block">
+                <div className="upper_info">
+                    <div className="temperature"> 20°</div>
+                    <div className="day"> Today</div>
+                </div>
+                <img src={logo} alt={alt}/>
+            </div>
+            <div className="bottom_block">
+                <div className="city">City:<span> Tallinn</span></div>
+                <div className="time">Time:<span> 19:32</span></div>
+            </div>
+        </div>
+    )
+}
